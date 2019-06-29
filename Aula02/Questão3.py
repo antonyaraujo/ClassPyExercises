2bef136aac2828040172e33692dd2e9729f52f6a
antonyaraujo <contato.antonyaraujo@gmail.com>
2019-06-29 16:54:30 -0300

    Inclusão do enunciado da questão 3

--- Aula02/Questão3.py
+++ Aula02/Questão3.py
@@ -1,3 +1,11 @@
+'''
+João recebeu seu salário e precisa pagar duas contas que estão
+atrasadas. João deverá pagar de multa 2% do valor de cada conta.
+Escreva um algoritmo que leia o valor do salário de João e o valor
+de cada uma das contas, calcule e mostre na tela quanto restará do
+salário de João.
+'''
+
 salario = float(input("João, informe seu salário: "))
 conta1 = float(input("João, informe o valor da conta 1: "))
 conta2 = float(input("João, informe o valor da conta 2: "))
